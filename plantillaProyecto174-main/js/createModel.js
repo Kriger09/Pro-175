0f64af45a2440d9de4730d422a38af05b069253d
Kriger09 <89879035+Kriger09@users.noreply.github.com>
2023-10-05 17:50:23 -0600

    Add files via upload

---
+++ plantillaProyecto174-main/js/createModel.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent("createmodels", {
+  init: async function() {
+    
+  },
+
+  //FUNCIÓN PARA OBTENER LOS DATOS DEL ARCHIVO JSON
+  getModels: function() {
+   
+    
+  },
+  
+  //FUNCIÓN PARA CREAR EL MODELO
+  createModel: function(model) {
+
+  }
+});
